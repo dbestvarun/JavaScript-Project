1b267cc4e102fdd90ff8e31f286d4438eaf4dc89
Varun Parashar <iamvarunparashar@gmail.com>
2024-08-26 17:15:36 +0530

    Added automated background color changing button

--- 1-colorChanger/script.js
+++ 1-colorChanger/script.js
@@ -1,6 +1,8 @@
 const buttons = document.querySelectorAll('.button');
 const body = document.body;
 // console.log(body)
+
+//Event added to the "button" class which changes the background color of the body as the bgColor of that element.
 buttons.forEach((button) => {
   button.addEventListener('click', (e) => {
     newColor = e.target.id;
@@ -8,3 +10,36 @@ buttons.forEach((button) => {
     body.style.backgroundColor = newColor;
   });
 });
+
+//function to generarte color hexcodes
+const newColor  = function(){
+  const hexaDecimals = "0123456789ABCDEF";
+  let colorCode = '#';
+  for(let i=0; i<6; i++){
+    let index = parseInt(Math.random()*16) %16;
+    colorCode += hexaDecimals[index];
+  }
+  return colorCode;
+}
+
+//variable to hold the reference of setInterval so that it can be used in clearInterval.
+let intervalId;
+
+//Event listeners added to the buttons.
+document.querySelector('#start').addEventListener('click',function(){
+  if(!intervalId){
+    intervalId = setInterval(() => {
+      let nextColor = newColor();
+      console.log(nextColor);
+      document.body.style.backgroundColor = nextColor;
+    }, 1000);
+  }
+}, false);
+
+document.querySelector('#stop').addEventListener('click',function(){
+  if(intervalId){
+    console.log("stopped.")
+    clearInterval(intervalId);
+    intervalId = null;
+  }
+},false);
